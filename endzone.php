c8fc543eb01532bfabf048a3f9ffd2a9cc0c16b5
kylebragger <kyle.bragger@gmail.com>
2010-07-30 09:57:10 -0400

    add header comments, DBAD license (thx zack and phil), die() if time ref isnt gmt

--- endzone.php
+++ endzone.php
@@ -1,10 +1,46 @@
 <?php
 
+/*
+
+                     DON'T BE A DICK PUBLIC LICENSE
+
+                       Version 1, December 2009
+
+    Copyright (C) 2009 Philip Sturgeon <email@philsturgeon.co.uk>
+
+    Everyone is permitted to copy and distribute verbatim or modified
+    copies of this license document, and changing it is allowed as long
+    as the name is changed.
+
+                     DON'T BE A DICK PUBLIC LICENSE
+       TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+     1. Do whatever you like with the original work, just don't be a dick.
+
+        Being a dick includes - but is not limited to - the following instances:
+
+    1a. Outright copyright infringement - Don't just copy this and change the name.
+    1b. Selling the unmodified original with no work done what-so-ever, that's REALLY being a dick.
+    1c. Modifying the original work to contain hidden harmful content. That would make you a PROPER dick.
+
+     2. If you become rich through modifications, related works/services, or supporting the original work,
+    share the love. Only a dick would make loads off this work and not buy the original works 
+    creator(s) a pint.
+
+     3. Code is provided with no warranty. Using somebody else's code and bitching when it goes wrong makes 
+    you a DONKEY dick. Fix the problem yourself. A non-dick would submit the fix back.
+
+*/
+
 /**
 * Endzone - Timezone happiness for CodeIgniter
 *
-* Kyle Bragger
-* Started July 30, 2010
+* Note that you must set the time_reference config to 'gmt' for maximum win.
+*
+* @author Kyle Bragger <kyle@forrst.com>
+* @package CodeIgniter
+* @subpackage Endzone
+* @since July 30, 2010
 *
 */
 
@@ -19,6 +55,8 @@ class Endzone {
     {
         $this->_ci =& get_instance();
         
+        if ($this->_ci->config->item('time_reference') != 'gmt') die('Endzone: Please set the CodeIgniter time_reference config to gmt');
+        
         $this->_ci->load->helper('date');
     }
     
@@ -26,6 +64,8 @@ class Endzone {
     * gmt_to_local()
     *
     * Takes a GMT timestamp and converts into a timestamp in the current timezone
+    *
+    * @param int $time a timestamp; defaults to now()
     *
     */
     public function gmt_to_local($time = null)
